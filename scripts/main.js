653787129599e8c4b2339e1866f686aaedb75976
Sébastien DEVAUX <devsebastien@hotmail.fr>
2018-01-11 18:45:28 +0100

    Switch sections

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,11 @@
 function toto() {
     document.getElementById('bouton').style.display = "none";
+    document.getElementById('panel').style.display = "block";
+
 }
 
 function foo() {
 document.getElementById('bouton').style.display = "block";
+document.getElementById('panel').style.display = "none";
+
 }
